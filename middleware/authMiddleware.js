8a2b3637b59dbde82baf711e0f773c7ab89246bb
Nandeesh-Gopal <nandeeshgopal26@gmail.com>
2026-02-06 21:12:44 +0530

    hmm

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,7 @@ export const protect = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer")) {
-    return res.status(401).json({ message: "Not authorized" });
+    return res.status(401).json({ message: "Not authorized " });
   }
 
   const token = authHeader.split(" ")[1];
